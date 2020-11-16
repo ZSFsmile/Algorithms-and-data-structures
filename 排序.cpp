8afb17bdf3b8db93b83aad457fbe2fe463241e9b
ZSFsmile <71601420+ZSFsmile@users.noreply.github.com>
2020-11-16 11:00:00 +0800

    Create 排序.cpp

---
+++ 排序.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubbleSort(int arr[],int len)//冒泡排序
+{
+    for(int i=0; i<len; i++)
+    {
+        bool flag=false;
+        for(int j=0; j<len-i-1; ++j)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                flag=true;
+            }
+        }
+        if(flag==false)
+            break;
+    }
+    return ;
+}
+
+void selectSort(int arr[],int len)//选择排序
+{
+    for(int i=0; i<len; i++)
+    {
+        int pos=i;
+        for(int j=i+1; j<len; ++j)
+        {
+            if(arr[j]<arr[pos])
+                pos=j;
+        }
+        swap(arr[i],arr[pos]);
+    }
+    return ;
+}
+
+void insertSort(int arr[],int len)//插入排序
+{
+    if(len<=1)
+        return;
+    for(int i=1; i<len; ++i)
+    {
+        int j=i-1;
+        int insertvalue=arr[i];
+        for(; j>=0; j--)
+        {
+            if(arr[j]>insertvalue)
+            {
+                arr[j+1]=arr[j];
+            }
+            else
+                break;
+        }
+        arr[j+1]=insertvalue;
+    }
+    return;
+}
+void quickSort(int arr[],int l,int r)//快速排序
+{
+    if(l>=r)
+        return;
+    int pivot=arr[r];///基准元素
+    int i=l;///i是两区间分界点,循环中arr[i]大于基准数
+    for(int j=l;j<r;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            swap(arr[i],arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i],arr[r]);
+    quickSort(arr,l,i-1);
+    quickSort(arr,i+1,r);
+    return ;
+}
+int main()
+{
+    int arr[1005];
+    int n;
+    scanf("%d",&n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d",&arr[i]);
+    }
+    quickSort(arr,0,n-1);
+    for(int i=0; i<n; ++i)
+    {
+        printf("%d ",arr[i]);
+    }
+}
