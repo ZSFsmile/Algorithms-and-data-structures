19be310cc9f7012eb536d7edf008f6a1dae887c5
ZhangShuaifeng <71601420+ZSFsmile@users.noreply.github.com>
2021-02-13 10:15:50 +0800

    Update 插入排序.cpp

--- 排序/插入排序.cpp
+++ 排序/插入排序.cpp
@@ -1,1 +1,21 @@
-
+void insertSort(int arr[],int len)//插入排序
+{
+    if(len<=1)
+        return;
+    for(int i=1; i<len; ++i)
+    {
+        int j=i-1;
+        int insertvalue=arr[i];
+        for(; j>=0; j--)
+        {
+            if(arr[j]>insertvalue)
+            {
+                arr[j+1]=arr[j];
+            }
+            else
+                break;
+        }
+        arr[j+1]=insertvalue;
+    }
+    return;
+}
