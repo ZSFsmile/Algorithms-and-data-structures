2029cdf66b4020ffeb7ae89d24a0357c45ca046f
ZhangShuaifeng <71601420+ZSFsmile@users.noreply.github.com>
2021-02-13 10:16:16 +0800

    Update 快速排序.cpp

--- 排序/快速排序.cpp
+++ 排序/快速排序.cpp
@@ -1,1 +1,19 @@
-
+void quickSolve(int arr[],int l,int r)//快速排序
+{
+    if(l>=r)
+        return;
+    int pivot=arr[r];///基准元素
+    int i=l;///i是两区间分界点,循环中arr[i]大于基准数
+    for(int j=l;j<r;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            swap(arr[i],arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i],arr[r]);
+    quickSolve(arr,l,i-1);
+    quickSolve(arr,i+1,r);
+    return ;
+}
