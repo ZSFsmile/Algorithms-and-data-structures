468beee0b02686f628a53cb5e27ba96b730e8bc7
ZhangShuaifeng <71601420+ZSFsmile@users.noreply.github.com>
2021-02-13 10:16:58 +0800

    Update 归并排序.cpp

--- 排序/归并排序.cpp
+++ 排序/归并排序.cpp
@@ -1,1 +1,14 @@
-
+void MergeSolve(int arr[],int l,int r) //归并排序
+{
+	if(l == r) return;
+	int mid((l+r)>>1);
+	MergeSolve(arr,l,mid);
+	MergeSolve(arr,mid+1,r);
+	inplace_merge(arr+l,arr+mid+1,arr+r+1);
+    return ;
+}
+void MergeSort(int arr[],int len)
+{
+    MergeSolve(arr,0,len-1);
+    return;
+}
