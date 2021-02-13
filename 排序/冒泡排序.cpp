1a7219849b2f387b12bd91bcdf4fdf6344ee89ac
ZhangShuaifeng <71601420+ZSFsmile@users.noreply.github.com>
2021-02-13 10:12:26 +0800

    Update 冒泡排序.cpp

--- 排序/冒泡排序.cpp
+++ 排序/冒泡排序.cpp
@@ -1,1 +1,18 @@
-
+void bubbleSort(int arr[],int len)//冒泡排序
+{
+    for(int i=0; i<len; i++)
+    {
+        bool flag=false;
+        for(int j=0; j<len-i-1; ++j)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                flag=true;
+            }
+        }
+        if(flag==false)
+            break;
+    }
+    return ;
+}
